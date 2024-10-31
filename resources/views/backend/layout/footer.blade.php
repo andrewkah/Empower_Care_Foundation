ac203f0bdf0416dff268a5dcd0895f95c8786599
andrewkah <107668588+andrewkah@users.noreply.github.com>
2024-10-31 23:29:11 +0300

    Updated minor issues

--- resources/views/backend/layout/footer.blade.php
+++ resources/views/backend/layout/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="footer-area">
-        <p>© Copyright 2024<a href="#">EmpowerCare</a>. All
+        <p>© Copyright 2024 <a href="#">EmpowerCare</a>. All
         Rights Reserved.</p>.</p>
     </div>
 </footer>
