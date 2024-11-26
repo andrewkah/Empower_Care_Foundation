d3b6833134bcac707a78a1fba77cb8c1d2d3cacb
kagsburg <kagsburg@gmail.com>
2024-11-26 19:34:33 +0300

    fixed issues on users and event categories

--- app/Http/Requests/EventRequest.php
+++ app/Http/Requests/EventRequest.php
@@ -26,7 +26,7 @@ class EventRequest extends FormRequest
             'summary' => 'required|string',
             'description' => 'nullable|string',
             'event_date' => 'required|date',
-            'event_time' => 'required|date_format:H:i:a',
+            'event_time' => 'required',
             'email' => 'required|email',
             'organizer' => 'required|string|max:255',
             'location' => 'required|string|max:255',
